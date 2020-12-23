f0ff97cb740a88e492f5a57edb82cbb5162f0081
Jenny Dinh <jenny@property.ca>
2020-12-23 09:02:09 -0700

    setting up

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,10 +5,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'An awesome blog about tech stuff' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap' }
     ]
   },
 
